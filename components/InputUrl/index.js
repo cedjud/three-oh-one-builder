58177b43dfa728ec83138c508bea42d725b90f88
Cedric Jud <cedric@apt.no>
2022-08-15 08:50:23 +0200

    Latest

--- components/InputUrl/index.js
+++ components/InputUrl/index.js
@@ -1,15 +1,30 @@
+import { useEffect } from 'react';
 import styles from './styles.module.css';
+
+import parseUrl from '../../lib/utils/parseUrl';
+import reduceUrlParts from '../../lib/utils/reduceUrlParts';
 
 import useSelectors from '../../lib/hooks/useSelectors';
 
 const InputUrl = ({url}) => {
-	const {urlIsMatched} = useSelectors(url);
+	const {hosts, paths, slugs, queries} = reduceUrlParts([parseUrl(url)]) 
+	const {urlIsMatched, matchedParts} = useSelectors(url);
+
+	// console.log('urlParts : ', urlParts);
+
+	useEffect(() => {
+	}, [matchedParts])
 
 	if (!urlIsMatched) {
 		return null
 	}
 	
-	return <li>{url}</li>;
+	return <li>
+		{hosts.map((host) => <span>{host}/</span>)}
+		{paths.map((path) => <span>{`${path} ` }/</span>)}
+		{slugs.map((slug) => <span>{`${slug} ` }/</span>)}
+		{queries.map((querie) => <span>{querie}</span>)}
+		</li>;
 }
 
 export default InputUrl;
